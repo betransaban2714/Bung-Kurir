87548a327b3e4fd1a740893cffe373759e3e58f1
Dede 18 <dedeeeeee18@gmail.com>
2026-05-24 13:05:00 +0000

    bahh keren skali, kasih naik 18%
    trus sa pu sistem ini de bisa baca in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -161,9 +161,9 @@ export default function Home() {
         ALAT TEMPUR KURIR INDONESIA TIMUR 🔥
       </div>
 
-      {/* WATERMARK BETRAN - Polos tanpa background, 8% transparansi */}
+      {/* WATERMARK BETRAN - Polos tanpa background, 18% transparansi */}
       <div className="fixed bottom-4 right-4 text-[10px] font-black flex items-center gap-1 pointer-events-none z-50">
-        <Copyright className="w-3 h-3 text-white/[0.08]" /> <span className="text-white/[0.08] tracking-wider">byBetranSaban</span>
+        <Copyright className="w-3 h-3 text-white/[0.18]" /> <span className="text-white/[0.18] tracking-wider">byBetranSaban</span>
       </div>
     </div>
   );
